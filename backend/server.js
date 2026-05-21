67b1afbdeb186dd71f5a5c749a4ce7565e8b34be
maris hamzah <hamzahmaris@gmail.com>
2026-05-21 22:34:18 +0500

    fix: bind server to 0.0.0.0 and fix misleading log message

--- backend/server.js
+++ backend/server.js
@@ -214,8 +214,8 @@ setInterval(() => {
   io.emit('ping');
 }, HEARTBEAT_INTERVAL);
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 export { io };
